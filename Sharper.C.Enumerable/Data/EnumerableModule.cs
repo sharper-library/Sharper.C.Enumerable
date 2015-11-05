8618c7bb06f5a92fb393530b2176ab0159c8aab9
Jeremy Hughes <jeremy@tflite.com>
2015-11-05 17:16:44 +1300

    More methods

--- Sharper.C.Enumerable/Data/EnumerableModule.cs
+++ Sharper.C.Enumerable/Data/EnumerableModule.cs
@@ -6,25 +6,96 @@ using Sharper.C.Control;
 namespace Sharper.C.Data
 {
 
+using static FunctionModule;
 using static TrampolineModule;
 
 public static class EnumerableModule
 {
-    public static B FoldLeft<A, B>(this IEnumerable<A> e, B x, Func<B, A, B> f)
+    public static IEnumerable<A> Seq<A>(params A[] items)
     =>
-        e.Aggregate(x, f);
+        items;
 
-    public static B FoldRight<A, B>(this IEnumerable<A> e, B x, Func<A, B, B> f)
+    public static IEnumerable<A> Seq1<A>(Func<A> fa)
+    {
+        yield return fa();
+    }
+
+    public static IEnumerable<A> Cons<A>(A x, IEnumerable<A> xs)
     =>
-        e.Reverse().Aggregate(x, (b, a) => f(a, b));
+        Seq(x).Concat(xs);
 
-    public static Trampoline<B> LazyFoldRight<A, B>
-      ( this IEnumerable<A> e
-      , B x
-      , Func<A, Trampoline<B>, Trampoline<B>> f
+    public static IEnumerable<A> Cons<A>(Func<A> x, IEnumerable<A> xs)
+    =>
+        Seq1(x).Concat(xs);
+
+    public static IEnumerable<A> Enumerate<A>(IEnumerator<A> e)
+    {
+        while (e.MoveNext())
+        {
+            yield return e.Current;
+        }
+    }
+
+    public static Func<IEnumerable<A>, B> MatchCons<A, B>
+      ( Func<B> nil
+      , Func<A, IEnumerable<A>, B> cons
       )
     =>
-        LazyFoldRight(e.GetEnumerator(), x, f);
+        xs =>
+        {
+            var e = xs.GetEnumerator();
+            return e.MoveNext()
+              ? cons(e.Current, Enumerate(e))
+              : nil();
+        };
+
+    public static IEnumerable<A> Forever<A>(A a)
+    {
+        while (true)
+        {
+            yield return a;
+        }
+    }
+
+    public static Func<A, IEnumerable<A>> Iterate<A>(Func<A, A> next)
+    =>
+        a => Iterate(next, a);
+
+    public static IEnumerable<int> Naturals
+    =>
+        Iterate((int a) => a + 1)(0);
+
+    public static IEnumerable<A> Flatten<A>(IEnumerable<IEnumerable<A>> xs)
+    =>
+        xs.SelectMany(Id);
+
+    public static Func<B, Func<IEnumerable<A>, B>>FoldLeft<A, B>
+      ( Func<B, A, B> f
+      )
+    =>
+        z => e => e.Aggregate(z, f);
+
+    public static Func<B, Func<IEnumerable<A>, B>> FoldRight<A, B>
+      ( Func<A, B, B> f
+      )
+    =>
+        z => e => e.Reverse().Aggregate(z, Flip(f));
+
+    public static Func<B, Func<IEnumerable<A>, Trampoline<B>>>
+    LazyFoldRight<A, B>
+      ( Func<A, Trampoline<B>, Trampoline<B>> f
+      )
+    =>
+        z => e => LazyFoldRight(e.GetEnumerator(), z, f);
+
+    private static IEnumerable<A> Iterate<A>(Func<A, A> next, A a)
+    {
+        while (true)
+        {
+            yield return a;
+            a = next(a);
+        }
+    }
 
     private static Trampoline<B> LazyFoldRight<A, B>
       ( IEnumerator<A> e
